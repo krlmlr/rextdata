2dc60e2182acad03368950ccd02b10c52ca72a15
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-02 21:08:59 +0100

    rename functions

--- R/extdata.R
+++ R/extdata.R
@@ -1,22 +1,22 @@
-extdata_path <- function(assign.env = assign.env) {
-  system.file("extdata", package = packageName(assign.env))
+extdata_path <- function(package.env = assign.env) {
+  system.file("extdata", package = packageName(package.env))
 }
 
-lazydata <- function(name, assign.env = parent.frame()) {
+extdata <- function(name, assign.env = parent.frame()) {
   name_ <- deparse(substitute(name, env = assign.env))
-  lazydata_(name_, assign.env = assign.env)
+  extdata_(name_, assign.env = assign.env)
 }
 
-lazydata_ <- function(name, assign.env = parent.frame()) {
+extdata_ <- function(name, assign.env = parent.frame()) {
   force(name)
   file_path <- file.path(extdata_path(assign.env), paste0(name, ".rds"))
   delayedAssign(name, { message("Loading ", file_path); readRDS(file_path) }, assign.env = assign.env)
   invisible(name)
 }
 
-all_lazy <- function(assign.env = parent.frame()) {
+auto_extdata() <- function(assign.env = parent.frame()) {
   extension_pattern <- "[.]rds$"
   files <- dir(extdata_path(assign.env), pattern = extension_pattern)
   names <- gsub(extension_pattern, "", files)
-  vapply(names, lazydata_, assign.env = assign.env, character(1L))
+  vapply(names, extdata_, assign.env = assign.env, character(1L))
 }
