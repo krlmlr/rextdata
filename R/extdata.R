38fad0b780547c78672683cc3f02be22a2bde740
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-12 23:11:51 +0100

    use local lazyforward, reorder documentation

--- R/extdata.R
+++ R/extdata.R
@@ -1,7 +1,38 @@
 #' Create a delayed assignment for a dataset
-#' @template se
+#'
+#' This function creates one or more delayed assignments.  It is focused on data
+#' stored in the \code{extdata} directory, and therefore especially useful for
+#' package development.  The main advantages over R's internal data loading
+#' mechanism are faster package build times; the delayed assignment makes sure
+#' that the data is loaded only if necessary.
+#'
+#' The "dots" argument can be named or unnamed; for unnamed arguments, names
+#' will be assigned by \code{\link[lazyeval]{all_dots}}.  For each argument,
+#' a delayed assignment with the specified name is created via
+#' \code{\link{delayedAssign}}.  The delayed assignment will evaluate the
+#' argument; if the argument is a single name, the corresponding \code{.rds}
+#' data file from the \code{extdata} directory will be loaded.
+#'
+#' The function \code{extdata_} is a standard-evaluation version of the
+#' above.  See the "lazyeval" vignette for details.
+#'
+#' @param ... Expressions for which delayed assignments are created.
+#' @param assign.env The environment in which to create the delayed assignments.
+#'
+#' @examples
+#' \dontrun{
+#' # Creates a delayed assignment "fortytwo" which loads the file
+#' # "extdata/fortytwo.rds"
+#' extdata(fortytwo = readRDS(system.file("extdata", "fortytwo.rds",
+#'         package = "mypackage")))
+#' # A shorter version of the above
+#' extdata(fortytwo)
+#' }
+#' @include lazyforward.R
+"extdata"
+
 #' @export
-#' @templateVar name extdata
+#' @rdname extdata
 extdata_ <- function(..., .dots, assign.env = parent.frame()) {
   dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
 
@@ -20,38 +51,8 @@ extdata_ <- function(..., .dots, assign.env = parent.frame()) {
   invisible(ret)
 }
 
-#' Create a delayed assignment for a dataset
-#'
-#' This function creates one or more delayed assignments.  It is focused on data
-#' stored in the \code{extdata} directory, and therefore especially useful for
-#' package development.  The main advantages over R's internal data loading
-#' mechanism are faster package build times; the delayed assignment makes sure
-#' that the data is loaded only if necessary.
-#'
-#' The "dots" argument can be named or unnamed; for unnamed arguments, names
-#' will be assigned by \code{\link[lazyeval]{all_dots}}.  For each argument,
-#' a delayed assignment with the specified name is created via
-#' \code{\link{delayedAssign}}.  The delayed assignment will evaluate the
-#' argument; if the argument is a single name, the corresponding \code{.rds}
-#' data file from the \code{extdata} directory will be loaded.
-#'
-#' The function \code{\link{extdata_}} is a standard-evaluation version of the
-#' above.  See the "lazyeval" vignette for details.
-#'
-#' @param ... Expressions for which delayed assignments are created.
-#' @param assign.env The environment in which to create the delayed assignments.
-#'
-#' @examples
-#' \dontrun{
-#' # Creates a delayed assignment "fortytwo" which loads the file
-#' # "extdata/fortytwo.rds"
-#' extdata(fortytwo = readRDS(system.file("extdata", "fortytwo.rds",
-#'         package = "mypackage")))
-#' # A shorter version of the above
-#' extdata(fortytwo)
-#' }
 #' @export
-extdata <- lazyforward::lazyforward("extdata_")
+extdata <- lazyforward("extdata_")
 
 
 extdata_one <- function(name, expr, assign.env = parent.frame()) {
