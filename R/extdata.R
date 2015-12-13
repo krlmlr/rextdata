eb298c5c0da45d73ce35a4a94bca0b382700dc6d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-13 02:31:02 +0100

    document

--- R/extdata.R
+++ R/extdata.R
@@ -1,32 +1,23 @@
 #' Create a delayed assignment for a dataset
 #'
-#' This function creates one or more delayed assignments.  It is focused on data
+#' This function creates one or more delayed assignments, focusing on data
 #' stored in the \code{extdata} directory, and therefore especially useful for
-#' package development.  The main advantages over R's internal data loading
-#' mechanism are faster package build times; the delayed assignment makes sure
-#' that the data is loaded only if necessary.
+#' package development.  The main advantage over R's internal data loading
+#' mechanism is faster package build time.
 #'
 #' The "dots" argument can be named or unnamed; for unnamed arguments, names
-#' will be assigned by \code{\link[lazyeval]{all_dots}}.  For each argument,
-#' a delayed assignment with the specified name is created via
-#' \code{\link{delayedAssign}}.  The delayed assignment will evaluate the
-#' argument; if the argument is a single name, the corresponding \code{.rds}
-#' data file from the \code{extdata} directory will be loaded.
+#' will be assigned by \code{\link[lazyeval]{all_dots}}.  The name of the
+#' RDS file is derived from the argument name and passed over to
+#' \code{\link{read_rds}}.
 #'
-#' The function \code{extdata_} is a standard-evaluation version of the
-#' above.  See the "lazyeval" vignette for details.
-#'
-#' @param ... Expressions for which delayed assignments are created.
-#' @param .dots Expressions as lazy objects.
+#' @param ... Base names for RDS files.
+#' @param .dots Base names for RDS files as lazy objects.
 #' @param assign.env The environment in which to create the delayed assignments.
 #'
 #' @examples
 #' \dontrun{
 #' # Creates a delayed assignment "fortytwo" which loads the file
 #' # "extdata/fortytwo.rds"
-#' extdata(fortytwo = readRDS(system.file("extdata", "fortytwo.rds",
-#'         package = "mypackage")))
-#' # A shorter version of the above
 #' extdata(fortytwo)
 #' }
 #' @include lazyforward.R
